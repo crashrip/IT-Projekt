7f3d8b7399d0db9d7826cfb2b8d53c6a9fb90319
crashrip <34278673+crashrip@users.noreply.github.com>
2018-02-02 13:05:02 +0100

    honhonhon

--- OLAP_WindowsForms/OLAP_WindowsForms.App/View/UserInput.cs
+++ OLAP_WindowsForms/OLAP_WindowsForms.App/View/UserInput.cs
@@ -397,7 +397,8 @@ namespace OLAP_WindowsForms.App
                 //  "SELECT DIM_PRED_NAME, DIM_PRED_SID " +
                 //  "FROM DW_DIM_PREDICATE " +
                 //  "WHERE LVL_SID = " + lvl_sid
-                "Select DIM_PRED_NAME, DIM_PRED_SID " +
+                
+                //"Select DIM_PRED_NAME, DIM_PRED_SID " +
                 "from dw_dim_predicate p inner join dw_level l on p.lvl_sid = l.lvl_sid where l.dim_sid = " + dim_sid + " and l.lvl_sid > 0 and "+
                 "l.lvl_position <= (select lvl_position from dw_level where lvl_sid = " + lvl_sid + ")");
                 DataTable dt2 = dt.Copy();
@@ -1151,24 +1152,17 @@ namespace OLAP_WindowsForms.App
                 // save ASS_SID 
                 if (ass_sid == -1)
                 {
-                    Console.WriteLine("safepoint 1");
                     DataTable dt = DBContext.Service().GetData(
                       "SELECT MAX(ASS_SID) FROM AGS_ANALYSIS_SITUATION_SCHEMA");
-                    Console.WriteLine("safepoint 2");
                     DataTable dt2 = dt.Copy();
-                    Console.WriteLine("safepoint 3");
                     DataRow[] dr = dt2.Select();
-                    Console.WriteLine("safepoint 4");
                     String index = dr[0].ItemArray[0].ToString();
-                    Console.WriteLine("safepoint 5 - index: "+index);
                     if (index.Equals("") || index == null)
                     {
-                        Console.WriteLine("table empty -> index 1");
                         id = 1;
                     }
                     else
                     {
-                        Console.WriteLine("safepoint 6");
                         id = Int32.Parse(index) + 1; // -> +1 for current ass_sid
                         Console.WriteLine("id: "+id);
                     }
