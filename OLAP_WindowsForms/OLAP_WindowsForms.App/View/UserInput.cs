358bee74e03b556d1e5d500cc674f5067ba74649
crashrip <34278673+crashrip@users.noreply.github.com>
2018-02-02 10:07:40 +0100

    final!°

--- OLAP_WindowsForms/OLAP_WindowsForms.App/View/UserInput.cs
+++ OLAP_WindowsForms/OLAP_WindowsForms.App/View/UserInput.cs
@@ -47,12 +47,11 @@ namespace OLAP_WindowsForms.App
         }
 
         // START ----- SelectNavigatinOperator class -------------------------------------------
-        public void SelectComboBoxCube(int selection) // TODO not yet working
+        public void SelectComboBoxCube(String selection) // TODO not yet working
         {
             Console.WriteLine("[SelectComboBoxCube] " + selection);
-            System.Threading.Thread.Sleep(5000);
-            //ComboBoxCube.SelectedIndex = ComboBoxCube.Items.IndexOf(selection);
-            ComboBoxCube.SelectedItem = selection;
+            ComboBoxCube.SelectedIndex = ComboBoxCube.FindString(selection);
+            comboBoxCube_SelectedIndexChanged(ComboBoxCube, new EventArgs());
         }
         // END ----- SelectNavigatinOperator class -------------------------------------------
 
